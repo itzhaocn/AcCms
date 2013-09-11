af68be6407464fa81a2dd669882b72cc7355dcd3
jyy12 <jyy12@qq.com>
2013-09-11 17:01:09 +0800

    ...
    ..........

--- public/javascripts/controlData.js
+++ public/javascripts/controlData.js
@@ -1,9 +1,17 @@
+//id名字与预览时有冲突，名称用mid
 var controlData={
+	"div":["mid","width","height","left","top"],
 	//链接
-	"link":["text","width","height","left","top","target","url"],
+	"link":["mid","text","width","height","left","top","target","url"],
+	"imgLink":["mid","img","text","width","height","left","top","target","url"],
 	//hover效果链接，img需填写完整background
-	"hoverLink":["text","width","height","left","top","target","url","img","hoverImg"],
-	"iframe":["width","height","left","top","src","scrolling","background","allowtransparency"]
+	"hoverLink":["mid","text","width","height","left","top","target","url","background","hoverBackground"],
+	"iframe":["mid","width","height","left","top","src","scrolling","background","allowtransparency"],
+	"flash":["mid","width","height","left","top","url"],
+	//微博textarea
+	"textarea":["mid","width","height","left","top","text","padding","background","color","border"],
+	//腾讯微博功能,需先添加一个文本域和按钮，以ID控制
+	"qqWeiboControl":["textareaId","btnId"]
 }
 
 
